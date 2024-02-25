d855a6f8fb59ab7aed0925119e71c0c88d345a10
nniabb <niabilanishvili17@gmail.com>
2024-02-25 22:36:54 +0400

    second commit

--- src/expense/expense.controller.ts
+++ src/expense/expense.controller.ts
@@ -1,1 +1,37 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { ExpenseService } from './expense.service';
+import { CreateExpenseDto } from './dto/create-expense.dto';
+import { UpdateExpenseDto } from './dto/update-expense.dto';
+
+
+@Controller('expense')
+export class ExpenseController {
+  constructor(private readonly expenseService: ExpenseService) {}
+
+  @Post()
+  create(@Body() createExpenseDto: CreateExpenseDto) {
+    return this.expenseService.create(createExpenseDto);
+  }
+
+  @Get()
+  findAll(@Query() queryParams) {
+    console.log('queryparams', queryParams)
+    return this.expenseService.findAll(queryParams);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.expenseService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateExpenseDto: UpdateExpenseDto) {
+    return this.expenseService.update(+id, updateExpenseDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.expenseService.remove(+id);
+  }
+}
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import * as morgan from 'morgan';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.use(morgan('tiny'));
-//   await app.listen(3000);
-// }
-// bootstrap();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(morgan('tiny'));
+  await app.listen(3000);
+}
+bootstrap();
